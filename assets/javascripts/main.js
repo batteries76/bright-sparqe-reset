f63b131614bf06fad466bd86babf11f12f06402e
Matt McKenzie <mattmckenzie@Matts-MacBook-Pro.local>
2018-12-10 22:12:45 +1100

    Navbar CSS (but not linked)

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -40,3 +40,21 @@ circles.forEach(circle => {
         circle.style.background = "#85c1e9"
     })
 })
+
+
+  // Look for .hamburger
+const hamburger = document.querySelector(".hamburger")
+const dropDown = document.querySelector(".nav-link-drop-area")
+
+hamburger.classList.toggle("is-active");
+
+// On click
+hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    // dropDown.style.background = "red"
+    dropDown.classList.toggle("no-display")
+
+})
+
